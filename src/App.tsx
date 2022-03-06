89b9716399323596c1c2cdaed6fbe110f4e7d8f4
sunny_thinkpad <sunnyw987@gmail.com>
2022-03-06 22:09:56 +1100

    rm depracated stuff :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { FunctionComponent } from "react";
 import { HashRouter } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
 import Routes from "./Routes";
 
 const App: FunctionComponent = () => (
 	<HashRouter>
-		<Navbar/>
+		{/* <Navbar/> */}
 		<Routes/>
-		<Footer/>
 	</HashRouter>
 )
 
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import HomePage from "./pages/HomePage/HomePage";
-import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 const Routes = () => (
     <Switch>
         <Route exact path ="/">
-            <HomePage/>
-        </Route>
-        <Route exact path ="/about">
-            <AboutPage/>
-        </Route>
-        <Route exact path ="/projects">
-            <ProjectsPage/>
+            <LandingPage />
         </Route>
         <Route exact path ="/*">
             <ErrorPage/>
